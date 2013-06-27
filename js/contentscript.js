70e4e27cab1644d6daa08da1794d1d2bb7c88b1f
Manuel Dell'Elce <mh025r0x@gmail.com>
2013-06-27 15:35:54 +0200

    Translate comments

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,16 +1,5 @@
-/**
- * Riceve i messaggi dall'injected script e se questi
- * sono per il panel, li gira al background che penserà al resto.
- * 
- * Nota: non è possibile far comunicare direttamente la pagina
- * con il panel in quanto si trovano in due "mondi" diversi, mentre
- * la pagina può comunicare con il content script tramite
- * l'invio di messaggi di HTML5.
- * Invece nell'altro verso il panel può accedere direttamente alla pagina
- * (tramite API dei devtools), per cui l'altro lato della comunicazione non è necessario.
- */
-
-// Messaggio dalla pagina, lo gira al background che farà il resto.
+// Receives messages from the inspected page and redirects them to the background,
+// building up the first step towards the communication between the backbone agent and the panel.
 window.addEventListener("message", function(event) {
     // We only accept messages from ourselves
     if (event.source != window) return;
@@ -19,11 +8,9 @@ window.addEventListener("message", function(event) {
 	chrome.extension.sendMessage(message);
 }, false);
 
-// Avvisa il panel quando la pagina è pronta 
-// (tipicamente utilizzato dal panel per aspettare il caricamento dell'agent
-//  e quindi controllare se questo è presente, cioè se la modalità debug è attiva)
+// Sends a message to the background when the DOM of the inspected page is ready
+// (typically used by the panel to check if the backbone agent is on the page).
 window.addEventListener('DOMContentLoaded', function() {
-	// il messaggio viene inviato al background che farà il resto
 	chrome.extension.sendMessage({
 		target: 'page',
 		name: 'ready'
