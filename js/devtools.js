b1d751400723877c47e7c9fa4eddf8012fedee9d
Manuel Dell'Elce <mh025r0x@gmail.com>
2013-11-07 15:36:12 +0100

    Change the name of Panel and Elements sidebar pane to 'Backbone'

--- js/devtools.js
+++ js/devtools.js
@@ -1,10 +1,10 @@
 // Script executed every time the devtools are opened.
 
 // custom panel
-chrome.devtools.panels.create("Backbone Debugger", "img/panel.png", "panel.html");
+chrome.devtools.panels.create("Backbone", "img/panel.png", "panel.html");
 
 // custom sidebar pane in the elements panel
-chrome.devtools.panels.elements.createSidebarPane("Backbone Debugger", function(sidebar) {
+chrome.devtools.panels.elements.createSidebarPane("Backbone", function(sidebar) {
     chrome.devtools.panels.elements.onSelectionChanged.addListener(function() {
         sidebar.setHeight("35px");
         sidebar.setPage("elementsSidebar.html");
