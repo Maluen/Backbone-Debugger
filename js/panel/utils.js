59f290155e059405bbdc77cded37e989cbe82c61
Manuel Dell'Elce <mh025r0x@gmail.com>
2013-12-17 19:53:01 +0100

    Add string utility functions

--- js/panel/utils.js
+++ js/panel/utils.js
@@ -17,6 +17,25 @@ define(["underscore", "jquery"], function(_, $) {
             $.ajax(requestObj).always(callback); // the callback is called also if the request fails
         };
 
+        // String utility functions.
+        this.string = {
+
+            // Based on http://stackoverflow.com/a/498995
+            trim: function(target) {
+                return target.replace(/^\s+|\s+$/g, '');
+            },
+
+            // Based on http://stackoverflow.com/a/1981366
+            removeMultipleSpaces: function(target) {
+                return target.replace(/\s{2,}/g, ' ');
+            },
+
+            // Return the simplified version of target, i.e. a trimmed string with multiple spaces removed
+            simplify: function(target) {
+                return this.removeMultipleSpaces(this.trim(target));
+            }
+        }
+
         this.initialize();
     })();
     return utils;
