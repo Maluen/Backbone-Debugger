45125a2e95dac02700ff55567d2b42516a58539a
Manuel Dell'Elce <mh025r0x@gmail.com>
2014-12-23 22:57:32 +0100

    Port filters to agent

---
+++ js/backboneAgent/filters/Filter.js
@@ -0,0 +1,36 @@
+Modules.set('filters.Filter', function() {
+    // imports
+    var Component = Modules.get('Component');
+
+    var Filter = Component.extend({
+
+        // function(model, attributeName)
+        // return boolean
+        match: undefined,
+
+        // If on is true, listen to the model change events in order to recheck the filter,
+        // calling callback with the info if the result changes. Also immediately calls the callback 
+        // with the initial result.
+        // If on is false, deactivate the live check.
+        liveMatch: function(model, on, callback) {
+            if (on) {
+                // activate
+                var result = this.match(model);
+                this.listenTo(model, "change", _.bind(function() {
+                    var newResult = this.match(model);
+                    if (newResult != result) {
+                        result = newResult;
+                        callback(model, newResult);
+                    }
+                }, this));
+                callback(model, result);
+            } else {
+                // deactivate
+                this.stopListening(model);
+            }
+        }
+
+    });
+
+    return Filter;
+});
---
+++ js/backboneAgent/filters/SearchFilter.js
@@ -0,0 +1,93 @@
+Modules.set('filters.SearchFilter', function() {
+    // imports
+    var Filter = Modules.get('filters.Filter');
+    var u = Modules.get('utils');
+
+    var SearchFilter = Filter.extend({
+
+        // Search term is a space separated list of "AND" search terms, e.g. "title foo"
+        setSearchTerm: function(searchTerm) {
+            // save the normalized search term
+            this.searchTerm = u.simplify(searchTerm);
+            if (u.startsWith(this.searchTerm, '"') && u.endsWith(this.searchTerm, '"')) {
+                this.strictSearch = true;
+                // remove quotes
+                this.searchTerm = u.removeBorders(this.searchTerm);
+            } else {
+                this.strictSearch = false;
+            }
+            // save the list with the search terms
+            this.searchTermList = this.searchTerm.split(" ");
+            // case insensitive search (turns terms to uppercase)
+            this.caseSensitive = false;
+            for (var i=0, l=this.searchTermList.length; i<l; i++) {
+                this.searchTermList[i] = this.searchTermList[i].toUpperCase();
+            }
+        },
+
+        // The method contains the actual filter logic.
+        // attributeName is an optional argument with the name of the attribute in which to perform the check,
+        // if omitted, the check will be performed on all the model attributes.
+        // Return true if the model matches the filter.
+        match: function(model, attributeName) {
+            if (attributeName !== undefined) {
+                return this.searchOnObjectProperty(model.attributes, attributeName, this.searchTermList);
+            }
+            return this.searchOnObject(model.attributes, this.searchTermList);
+        },
+
+        // return true if each term appears in at least one object property (name or value)
+        // object can be an actual object or an array.
+        searchOnObject: function(object, terms) {
+            if (u.isArray(object)) {
+                for (var i=0, l=object.length; i<l; i++)
+                    if (this.searchOnObjectProperty(object, i, terms)) return true;
+            } else {
+                for (property in object)
+                    if (object.hasOwnProperty(property))
+                        if (this.searchOnObjectProperty(object, property, terms)) return true;
+            }
+            return false;
+        },
+
+        // return true if each term appears in the object property (name or value)
+        searchOnObjectProperty: function(object, property, terms) {
+            for (var i=0, l=terms.length; i<l; i++) {
+                if (!this.searchTermOnObjectProperty(object, property, terms[i])) return false;
+            }
+            return true;
+        },
+
+        // return true if the term appears in the object property (name or value)
+        searchTermOnObjectProperty: function(object, property, term) {
+            // check into the property name (if it's not an array index)
+            if (!u.isArray(object) && this.searchTermOnObjectPropertyString(String(property), term)) return true;
+            // check into the property value
+            var value = object[property];
+            if (!u.isObject(value)) {
+                if (this.searchTermOnObjectPropertyString(String(value), term)) return true;
+            } else {
+                // recursive check
+                if (this.searchOnObject(value, [term])) return true;
+            }
+            return false;
+        },
+
+        searchTermOnObjectPropertyString: function(property, term) {
+            if (!this.caseSensitive) property = property.toUpperCase(); // case insensitive search
+            if (this.strictSearch) {
+                // returns true if there exists a 'word' in the property that is equal to the search term
+                var words = property.split(' ');
+                for (var i=0, l=words.length; i<l; i++) {
+                    if (words[i] == term) return true;
+                }
+            } else {
+                // returns true if the property contains the term
+                return property.indexOf(term) != -1;
+            }
+        }
+
+    });
+
+    return SearchFilter;
+});
