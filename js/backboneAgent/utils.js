939ffea3f07277fab122bbaed6685072cdd80726
Manuel Dell'Elce <mh025r0x@gmail.com>
2014-12-23 22:56:02 +0100

    Port string utils to agent (they will be required by search filter)

--- js/backboneAgent/utils.js
+++ js/backboneAgent/utils.js
@@ -215,7 +215,41 @@ Modules.set('utils', function() {
             } else {
                 this.patchFunction(object, functionName, patcher);
             }
+        },
+
+        // String utility functions
+
+        // Based on http://stackoverflow.com/a/498995
+        trim: function(target) {
+            return target.replace(/^\s+|\s+$/g, '');
+        },
+
+        // Based on http://stackoverflow.com/a/1981366
+        removeMultipleSpaces: function(target) {
+            return target.replace(/\s{2,}/g, ' ');
+        },
+
+        // Return the simplified version of target, i.e. a trimmed string with multiple spaces removed
+        simplify: function(target) {
+            return this.removeMultipleSpaces(this.trim(target));
+        },
+
+        // Based on http://stackoverflow.com/a/646643
+        startsWith: function(target, str) {
+            return target.slice(0, str.length) == str;
+        },
+        endsWith: function(target, str) {
+            return target.slice(-str.length) == str;
+        },
+
+        // remove first and last char
+        removeBorders: function(target) {
+            target = target.substring(1); // first
+            target = target.substring(0, target.length-1); // last
+            return target;
         }
+
+        // end String utility functions
 
     };
 
