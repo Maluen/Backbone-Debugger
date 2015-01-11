cb80cfb19ec7100653c1d3a7be960a2379477335
Manuel Dell'Elce <mh025r0x@gmail.com>
2015-01-11 01:37:34 +0100

    Refactor content script as singleton object

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,111 +1,118 @@
-// as per chrome, frameURL is the original frame url without eventual hash string
-var frameURL = window.location.origin + window.location.pathname + window.location.search;
+var contentScript = new (function() { // singleton
 
-var isTopFrame = (window.parent == window);
+	// as per chrome, frameURL is the ORIGINAL frame url without eventual hash string
+	this.frameURL = window.location.origin + window.location.pathname + window.location.search;
+	this.isTopFrame = (window.parent == window);
 
-// Setup an Inspected page <-> Content script peer to peer MessageChannel connection.
-// Since we don't know which of the two will start first, they both do the same thing:
-// on start we send the started message, if the other didn't started yet, then the message will
-// never be received by it, but in that case we'll receive its started message, 
-// we'll then send the connect message since we're now sure that the other is ready to receive
-// our port (it can only be sent once since the object ownership is transferred (neutered)),
-// and we'll resend also the started message that will be received too for sure;
-// the other end will thus receive the connect message, to which it reacts
-// by listening to the port, and the started message, to which reacts
-// by sending its connect message and its started message.
-// Finally, we'll receive the connect message, to which we react by listening,
-// and the started message again, that we'll just ignore.
-// In the meantime, they both can send messages to the out ports, even if the other didn't
-// received it yet, since the MessageChannel port start() method emits all the messages
-// received until that moment.
+	// Setup an Inspected page <-> Content script peer to peer MessageChannel connection.
+	// Since we don't know which of the two will start first, they both do the same thing:
+	// on start we send the started message, if the other didn't started yet, then the message will
+	// never be received by it, but in that case we'll receive its started message, 
+	// we'll then send the connect message since we're now sure that the other is ready to receive
+	// our port (it can only be sent once since the object ownership is transferred (neutered)),
+	// and we'll resend also the started message that will be received too for sure;
+	// the other end will thus receive the connect message, to which it reacts
+	// by listening to the port, and the started message, to which reacts
+	// by sending its connect message and its started message.
+	// Finally, we'll receive the connect message, to which we react by listening,
+	// and the started message again, that we'll just ignore.
+	// In the meantime, they both can send messages to the out ports, even if the other didn't
+	// received it yet, since the MessageChannel port start() method emits all the messages
+	// received until that moment.
 
-var isConnectedWithPage = false;
-var channel;
-var pagePortOut;
-var pagePortIn;
+	this.isConnectedWithPage = false;
+	this.channel = undefined;
+	this.pagePortOut = undefined;
+	this.pagePortIn = undefined;
 
-var initialize = function() {
-	channel = new MessageChannel();
-	pagePortOut = channel.port1;
+	this.initialize = function() {
+		this.channel = new MessageChannel();
+		this.pagePortOut = this.channel.port1;
 
-	waitForPage();
-	notifyStartToPage();
-}
+		this.waitForPage();
+		this.notifyStartToPage();
 
-var waitForPage = function() {
-	window.addEventListener('message', function(event) {
-	    // Only accept messages from same frame
-	    if (event.source != window) return;
+		this.listenToBackground();
 
-	    var message = event.data;
+		if (this.isTopFrame) this.ifTopFrame();
+	};
 
-	    // Only accept our messages
-	    if (typeof message != 'object' || message === null || message.target != 'page') return;
+	this.waitForPage = function() {
+		window.addEventListener('message', (function(event) {
+		    // Only accept messages from same frame
+		    if (event.source != window) return;
 
-	    if (message.name == 'started' && !isConnectedWithPage) {
-	    	// now we are sure the other is ready to receive our port
-	    	connectToPage();
+		    var message = event.data;
 
-	    	// this is needed if the content script started before the page
-	    	// (in that case the inspected page never received the initial 'active' message)
-	    	notifyStartToPage();
-	    } else if (message.name == 'connect' && !pagePortIn) {
-	    	pagePortIn = event.ports[0];
+		    // Only accept our messages
+		    if (typeof message != 'object' || message === null || message.target != 'page') return;
 
-	    	listenToPage();
-	    }
-	});
-}
+		    if (message.name == 'started' && !this.isConnectedWithPage) {
+		    	// now we are sure the other is ready to receive our port
+		    	this.connectToPage();
 
-var notifyStartToPage = function() {
-	window.postMessage({
-		target: 'extension',
-		timestamp: new Date().getTime(),
-		name: 'started'
-	}, '*');
-}
+		    	// this is needed if the content script started before the page
+		    	// (in that case the inspected page never received the initial 'active' message)
+		    	this.notifyStartToPage();
+		    } else if (message.name == 'connect' && !this.pagePortIn) {
+		    	this.pagePortIn = event.ports[0];
 
-// this must be called only once since the port ownership is transferred (neutered) !
-var connectToPage = function(connectMessage) {
-	window.postMessage({
-		target: 'extension',
-		timestamp: new Date().getTime(),
-		name: 'connect'
-	}, [channel.port2], '*');
+		    	this.listenToPage();
+		    }
+		}).bind(this));
+	};
 
-	isConnectedWithPage = true;
-}
+	this.notifyStartToPage = function() {
+		window.postMessage({
+			target: 'extension',
+			timestamp: new Date().getTime(),
+			name: 'started'
+		}, '*');
+	};
 
-// Listens to inspected page frame messages and redirects them to the background,
-// building up the first step towards the communication between the backbone agent and the panel.
-var listenToPage = function() {
-	pagePortIn.addEventListener('message', function(event) {
-		var message = event.data;
-		message.frameURL = frameURL;
-		chrome.extension.sendMessage(message);
-	});
-	pagePortIn.start();
-}
+	// this must be called only once since the port ownership is transferred (neutered)!
+	this.connectToPage = function() {
+		window.postMessage({
+			target: 'extension',
+			timestamp: new Date().getTime(),
+			name: 'connect'
+		}, [this.channel.port2], '*');
 
-// Receives messages from the background and redirects them to the inspected page.
-chrome.runtime.onMessage.addListener(function(message) {
-	// Only accept messages for our frame
-	if (message.frameURL != frameURL) return;
+		this.isConnectedWithPage = true;
+	};
 
-	pagePortOut.postMessage(message);
-});
+	// Listens to inspected page frame messages and redirects them to the background,
+	// building up the first step towards the communication between the backbone agent and the panel.
+	this.listenToPage = function() {
+		this.pagePortIn.addEventListener('message', (function(event) {
+			var message = event.data;
+			message.frameURL = this.frameURL;
+			chrome.extension.sendMessage(message);
+		}).bind(this));
+		this.pagePortIn.start();
+	};
 
-if (isTopFrame) {
-	/* Code to be executed only if this is the top frame content script! */
+	// Receives messages from the background and redirects them to the inspected page.
+	this.listenToBackground = function() {
+		chrome.runtime.onMessage.addListener((function(message) {
+			// Only accept messages for our frame
+			if (message.frameURL != this.frameURL) return;
 
-	// Sends a message to the background when the DOM of the inspected page is ready
-	// (typically used by the panel to check if the backbone agent is on the page).
-	window.addEventListener('DOMContentLoaded', function() {
-	    chrome.extension.sendMessage({
-	        target: 'page',
-	        name: 'ready'
-	    });
-	}, false);
-}
+			this.pagePortOut.postMessage(message);
+		}).bind(this));
+	};
 
-initialize();
+	// Code to be executed only if this is the top frame content script!
+	this.ifTopFrame = function() {
+		// Sends a message to the background when the DOM of the inspected page is ready
+		// (typically used by the panel to check if the backbone agent is on the page).
+		window.addEventListener('DOMContentLoaded', function() {
+		    chrome.extension.sendMessage({
+		        target: 'page',
+		        name: 'ready'
+		    });
+		}, false);
+	};
+
+	this.initialize();
+})();
