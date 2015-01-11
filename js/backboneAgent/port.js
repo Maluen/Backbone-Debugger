fe79cb27d4d4c2708271d59c15c07c1a292b130b
Manuel Dell'Elce <mh025r0x@gmail.com>
2015-01-11 01:06:05 +0100

    Use p2p agent-contentScript MessageChannel connection This creates a dedicated communication channel for agent and content script, rmoving conflicts that might arise with other libraries (e.g. facebook) when posting normal messages on the window object itself; moreover, this removes the message 'feedback' behaviour, where the message is received also by the one that sent it; no need for origin checks.

--- js/backboneAgent/port.js
+++ js/backboneAgent/port.js
@@ -5,11 +5,20 @@ Modules.set('port', function() {
 
 	var port = new (Component.extend({ // singleton
 
+		isConnectedWithExtension: false,
+		channel: undefined,
+		extensionPortOut: undefined,
+		extensionPortIn: undefined,
+
 		initialize: function() {
-			this.startListening();
+			this.channel = new MessageChannel();
+			this.extensionPortOut = this.channel.port1;
+
+			this.waitForExtension();
+			this.notifyStartToExtension();
 		},
 
-		startListening: function() {
+		waitForExtension: function() {
 			window.addEventListener('message', u.bind(function(event) {
 			    // Only accept messages from same frame
 			    if (event.source != window) return;
@@ -19,8 +28,40 @@ Modules.set('port', function() {
 			    // Only accept our messages
 			    if (!u.isObject(message) || message.target != 'extension') return;
 
-			    this.trigger(message.name, message);
+			    if (message.name == 'started' && !this.isConnectedWithExtension) {
+			    	this.connectToExtension();
+			    	this.notifyStartToExtension();
+			    } else if (message.name == 'connect' && !this.extensionPortIn) {
+			    	this.extensionPortIn = event.ports[0];
+			    	this.listenToExtension();
+			    }
 			}, this));
+		},
+
+		notifyStartToExtension: function() {
+			window.postMessage({
+				target: 'page',
+				timestamp: new Date().getTime(),
+				name: 'started'
+			}, '*');
+		},
+
+		connectToExtension: function() {
+			window.postMessage({
+				target: 'page',
+				timestamp: new Date().getTime(),
+				name: 'connect'
+			}, [this.channel.port2], '*');
+
+			this.isConnectedWithExtension = true;
+		},
+
+		listenToExtension: function() {
+			this.extensionPortIn.addEventListener('message', u.bind(function(event) {
+				var message = event.data;
+				this.trigger(message.name, message);
+			}, this));
+			this.extensionPortIn.start();
 		},
 
 		// Note: messageName is prefixed by "backboneAgent:" and can't contain spaces
@@ -28,12 +69,12 @@ Modules.set('port', function() {
 		sendMessage: function(messageName, messageData) {
 		    messageName = 'backboneAgent:'+messageName;
 
-		    window.postMessage({
+		    this.extensionPortOut.postMessage({
 		        target: 'page',
 		        timestamp: new Date().getTime(),
 		        name: messageName,
 		        data: messageData
-		    }, '*');
+		    });
 		}
 
 	}))();
--- js/contentscript.js
+++ js/contentscript.js
@@ -3,27 +3,96 @@ var frameURL = window.location.origin + window.location.pathname + window.locati
 
 var isTopFrame = (window.parent == window);
 
-// Receives messages from the inspected page frame and redirects them to the background,
+// Setup an Inspected page <-> Content script peer to peer MessageChannel connection.
+// Since we don't know which of the two will start first, they both do the same thing:
+// on start we send the started message, if the other didn't started yet, then the message will
+// never be received by it, but in that case we'll receive its started message, 
+// we'll then send the connect message since we're now sure that the other is ready to receive
+// our port (it can only be sent once since the object ownership is transferred (neutered)),
+// and we'll resend also the started message that will be received too for sure;
+// the other end will thus receive the connect message, to which it reacts
+// by listening to the port, and the started message, to which reacts
+// by sending its connect message and its started message.
+// Finally, we'll receive the connect message, to which we react by listening,
+// and the started message again, that we'll just ignore.
+// In the meantime, they both can send messages to the out ports, even if the other didn't
+// received it yet, since the MessageChannel port start() method emits all the messages
+// received until that moment.
+
+var isConnectedWithPage = false;
+var channel;
+var pagePortOut;
+var pagePortIn;
+
+var initialize = function() {
+	channel = new MessageChannel();
+	pagePortOut = channel.port1;
+
+	waitForPage();
+	notifyStartToPage();
+}
+
+var waitForPage = function() {
+	window.addEventListener('message', function(event) {
+	    // Only accept messages from same frame
+	    if (event.source != window) return;
+
+	    var message = event.data;
+
+	    // Only accept our messages
+	    if (typeof message != 'object' || message === null || message.target != 'page') return;
+
+	    if (message.name == 'started' && !isConnectedWithPage) {
+	    	// now we are sure the other is ready to receive our port
+	    	connectToPage();
+
+	    	// this is needed if the content script started before the page
+	    	// (in that case the inspected page never received the initial 'active' message)
+	    	notifyStartToPage();
+	    } else if (message.name == 'connect' && !pagePortIn) {
+	    	pagePortIn = event.ports[0];
+
+	    	listenToPage();
+	    }
+	});
+}
+
+var notifyStartToPage = function() {
+	window.postMessage({
+		target: 'extension',
+		timestamp: new Date().getTime(),
+		name: 'started'
+	}, '*');
+}
+
+// this must be called only once since the port ownership is transferred (neutered) !
+var connectToPage = function(connectMessage) {
+	window.postMessage({
+		target: 'extension',
+		timestamp: new Date().getTime(),
+		name: 'connect'
+	}, [channel.port2], '*');
+
+	isConnectedWithPage = true;
+}
+
+// Listens to inspected page frame messages and redirects them to the background,
 // building up the first step towards the communication between the backbone agent and the panel.
-window.addEventListener("message", function(event) {
-    // Only accept messages from same frame
-    if (event.source != window) return;
-
-    var message = event.data;
-
-    // Only accept our messages
-    if (typeof message != 'object' || message === null || message.target != 'page') return;
-
-    message.frameURL = frameURL;
-    chrome.extension.sendMessage(message);
-}, false);
+var listenToPage = function() {
+	pagePortIn.addEventListener('message', function(event) {
+		var message = event.data;
+		message.frameURL = frameURL;
+		chrome.extension.sendMessage(message);
+	});
+	pagePortIn.start();
+}
 
 // Receives messages from the background and redirects them to the inspected page.
 chrome.runtime.onMessage.addListener(function(message) {
 	// Only accept messages for our frame
 	if (message.frameURL != frameURL) return;
 
-	window.postMessage(message, '*');
+	pagePortOut.postMessage(message);
 });
 
 if (isTopFrame) {
@@ -38,3 +107,5 @@ if (isTopFrame) {
 	    });
 	}, false);
 }
+
+initialize();
